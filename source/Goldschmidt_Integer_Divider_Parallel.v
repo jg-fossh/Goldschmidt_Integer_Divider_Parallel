aff098abb30c87978770536b07e3dc83832fa986
jota <jg-fossh@protonmail.com>
2022-11-01 22:32:01 -0400

    Updating license

--- source/Goldschmidt_Integer_Divider_Parallel.v
+++ source/Goldschmidt_Integer_Divider_Parallel.v
@@ -1,37 +1,16 @@
 /////////////////////////////////////////////////////////////////////////////////
-// BSD 3-Clause License
-//
-// Copyright (c) 2022, Jose R. Garcia
+// 
+// Copyright (c) 2022, Jose R. Garcia (jg-fossh@protonmail.com)
 // All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-// 1. Redistributions of source code must retain the above copyright notice, this
-//    list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-//    this list of conditions and the following disclaimer in the documentation
-//    and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the copyright holder nor the names of its
-//    contributors may be used to endorse or promote products derived from
-//    this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// The following hardware description source code is subject to the terms of the
+//                  Open Hardware Description License, v. 1.0
+// If a copy of the afromentioned license was not distributed with this file you
+// can obtain one at http://juliusbaxter.net/ohdl/ohdl.txt
 //
 /////////////////////////////////////////////////////////////////////////////////
 // File name    : Goldschmidt_Integer_Divider_Parallel.v
-// Author       : Jose R Garcia
+// Author       : Jose R Garcia (jg-fossh@protonmail.com)
 // Project Name : Goldschmidt Integer Divider Parallel
 // Module Name  : Goldschmidt_Integer_Divider_Parallel
 // Description  : The Goldschmidt divider is an iterative method
@@ -42,12 +21,12 @@
 //   Suggested values for 32bit integer division
 //     P_GDIV_FACTORS_MSB = 31,                   
 //     P_GDIV_FRAC_LENGTH = P_GDIV_FACTORS_MSB+1,           
-//     P_GDIV_ROUND_LVL   = 2 to 4                    
+//     P_GDIV_ROUND_LVL   = 3                   
 /////////////////////////////////////////////////////////////////////////////////
 module Goldschmidt_Integer_Divider_Parallel #(
-  parameter integer P_GDIV_FACTORS_MSB = 31,                   // The MSB of each division factor.
+  parameter integer P_GDIV_FACTORS_MSB = 7,                    // The MSB of each division factor.
   parameter integer P_GDIV_FRAC_LENGTH = P_GDIV_FACTORS_MSB+1, // he amount of bits after the fixed point.
-  parameter integer P_GDIV_ROUND_LVL   = 3                     // Bits after fixed point that need to be '1' to round up result.
+  parameter integer P_GDIV_ROUND_LVL   = 2                     // Bits after fixed point that need to be '1' to round up result.
 )(
   // Component's clocks and resets
   input i_clk, // clock
@@ -355,7 +334,9 @@ module Goldschmidt_Integer_Divider_Parallel #(
 
   ///////////////////////////////////////////////////////////////////////////////
   // Process     : EE_LUT_Entry_Select
-  // Description : Creates and selects the correct entry in the LUT.
+  // Description : Creates a mux with the possiible 10^-n values given the size 
+  //               of the input vectors and selects the correct that will 'shift'
+  //               the inputs decimal point so that it is less the 2.0
   ///////////////////////////////////////////////////////////////////////////////
   always @(*) begin : EE_LUT_Entry_Select
     // Creates a check of the input against the 2EEx to select the LUT entry
@@ -380,9 +361,13 @@ module Goldschmidt_Integer_Divider_Parallel #(
   /////////////////////////////////////////////////////////////////////////////
   always @(posedge i_clk) begin : Division_Step_Process
     if (r_stall == 1'b0) begin
-        r_div_step <= 1;
+      // Stall is asserted when the FSM enters the iterative states,
+      // Hence when not stalling ready the signal.
+      r_div_step <= 1;
     end
     else begin
+      // In the itrative steps, push 1s in to detect when the compile time 
+      // determined convergence occurs.
       r_div_step <= r_div_step << 1;
     end
   end // Division_Step_Process
